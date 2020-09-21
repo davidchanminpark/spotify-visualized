87db79076790fbd8c6a4ce6d096301902ea8f5e0
Chanmin Park <davidpark0315@gmail.com>
2020-09-21 00:09:19 -0400

    make new server folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
 import React, { useState } from "react";
 import "./App.css";
 import axios from "axios";
-import { Layout, Avatar, Button } from "antd";
 import { Switch, Route } from "react-router-dom";
 import SignUp from './SignUp';
 import Home from './Home';
-
-const { Header, Content, Footer } = Layout;
 
 function App() {
 
---
+++ server/credentials.js
@@ -0,0 +1,5 @@
+var client_id = '27f42c7a5bd2422085274fdab669374a'; // Your client id
+var client_secret = '29b1cdd7dc2443c9b8ff6f1cb99def13'; // Your secret
+
+exports.client_id = client_id; 
+exports.client_secret = client_secret; 
